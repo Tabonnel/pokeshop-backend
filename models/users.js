2f942a4c4c903dba9e372fb280504fda8d6ea836
Thibaud <abonnel.t@gmail.com>
2023-09-05 17:43:03 +0200

    wishList

--- models/users.js
+++ models/users.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+   
 
 const userSchema = mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-    token: String,
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+  wishList: [{type: mongoose.Schema.Types.ObjectId, ref: "pokemons"}],
 });
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
